c10c45c0663b5dc6824ed8f70c6ed9835a9860e8
Xin Zheng <jason.zhengxin@gmail.com>
2016-04-06 00:34:03 +1000

    project set up

---
+++ src/client/app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xin on 6/04/2016.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello, world</h1>,
+    document.getElementById('example')
+);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by xin on 6/04/2016.
+ */
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    entry: APP_DIR + '/app.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
