db0d2f360cda40372b82be133bfb0f7487c9be4f
Xin Zheng <jason.zhengxin@gmail.com>
2016-04-10 22:05:22 +1000

    the menu focus state is added

--- src/app/js/Menu.js
+++ src/app/js/Menu.js
@@ -4,28 +4,57 @@ import {grey50} from '../../../node_modules/material-ui/lib/styles/colors';
 
 import Menu from '../../../node_modules/material-ui/lib/menus/menu';
 import MenuItem from '../../../node_modules/material-ui/lib/menus/menu-item';
+//
+// function handleMenuChange(e) {
+//     console.log(this);
+//     console.log("this on menu", this);
+//
+// }
 
+MenuItem.state = {focused: 0};
+
+function handleOnTouchTap(index) {
+     this.setState ({focused: index});
+    // this.focusState = 'focused';
+    //  e.currentTarget.className = 'focused';
+}
 
 const itemStyle = {
-    float: 'left',
-    position: 'relative',
-    zIndex: 0,
-    display: 'inline',
-    color: grey50
+        float: 'left',
+        position: 'relative',
+        zIndex: 0,
+        display: 'inline',
+        paddingTop: '8px',
+        paddingBottom: '8px',
+        color: grey50
 };
 
+const menuItems = [
+    {title: 'Home', value: 'home'},
+    {title: 'Portfolio', value: 'portfolio'},
+    {title: 'Project', value: 'project'},
+    {title: 'Contact Us', value: 'contact'}
+]
 
 
 const MenuComponent = () => (
-    <div>
-        <Menu>
-            <a href="#" id="logo" title="Mansion Village"></a>
-            <MenuItem primaryText="Home" style={itemStyle} />
-            <MenuItem primaryText="Portfolio" style={itemStyle}/>
-            <MenuItem primaryText="Project" style={itemStyle}/>
-            <MenuItem primaryText="Contact Us" style={itemStyle}/>
+        <Menu desktop={true} autoWidth={true} >
+            <a href="#" id="logo" title="Mansion Village">&nbsp;</a>
+            {
+
+                menuItems.map(function(val, index){
+                    let style = '';
+
+                    if(MenuItem.state.focused == index) {
+                        style = 'focused';
+                    }
+                      return  <MenuItem className={style} key={index} primaryText={ val.title } style={itemStyle} value={val.value} onTouchTap={handleOnTouchTap.bind(MenuItem, index)}/>
+
+                    }
+
+                )
+            }
         </Menu>
-    </div>
 );
 
 export default MenuComponent;
